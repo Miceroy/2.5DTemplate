f730c85136c45b8e034345222f76f049e9e2ba28
Mikko Romppainen <mikko.romppainen@kamk.fi>
2018-03-24 17:35:26 +0200

    Initial version.

---
+++ Assets/PlayerCharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour {
+	public float speed = 2.0f;
+	Vector3 velExtra = new Vector3(0,0,0);
+	Transform tr;
+
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent<Transform> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vh = Input.GetAxis("Horizontal");
+		float vv = Input.GetAxis("Vertical");
+		// Compute velocity
+		Vector3 velocity = speed * (new Vector3(vh,0,vv)).normalized;
+		// Integrate new position
+		tr.position = tr.position + (velocity+velExtra)*Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("OnTriggerEnter");
+		velExtra = 2.0f * speed * (transform.position - other.transform.position).normalized;
+		velExtra.y = 0;
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("OnTriggerExit");
+		velExtra.Set (0, 0, 0);
+	}
+}
---
+++ Assets/SpriteController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour {
+	public SpriteRenderer sr;
+	private Transform tr;
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int layerNum = Mathf.RoundToInt(tr.position.z);
+		sr.sortingLayerName = "L" + layerNum.ToString ();
+	}
+}
